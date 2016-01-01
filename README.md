508a9d207341e615c26beceb446b85538b4191a8
vau <68410627+vauSoftTech@users.noreply.github.com>
2016-01-01 10:11:00 +0530

    A few updates

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# vau_widgets
+# vau widgets
+**tKinter / ttk Widgets**
+
+
+* DateEntry Widget 
+A compound widget inherited from Frame. Facilitates easy handling of Entry of Python Date.
+  
+* TimeEntry Widget 
+A compound widget inherited from Frame. Facilitates easy handling of Entry of Python Time.
--- src/vau_widgets/__init__.py
+++ src/vau_widgets/__init__.py
@@ -1,2 +1,27 @@
-import .dateentry
-import .timeentry
+#!/usr/bin/env python3
+# -*- coding: utf_8 -*-
+"""
+
+    Copyright    : 2016 January. A. R. Bhatt.
+    Organization : VAU SoftTech
+    Project      : Date & Time Entry Components for tKinter & TTK
+    Script Name  : __init__.py
+    License      : GNU General Public License v3.0
+
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+from .dateentry import *
+from .timeentry import *
